5ff99dcae6ce0681ec8d9dc8dd8f25f1bb8801b7
Michael Dorst <mikefdorst@gmail.com>
2012-12-28 22:57:07 -0800

    Initial Commit

---
+++ glSnake/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  glSnake
+//
+//  Created by Michael Dorst on 12/28/12.
+//  Copyright (c) 2012 Michael Dorst. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
